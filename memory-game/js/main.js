203a17865285563299c881e6003f634fa5934e8d
aaronl647 <AaronLei@Aarons-MacBook-Pro.local>
2020-02-09 18:54:22 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,4 +1,4 @@
-const cards = [
+var cards = [
 {
 	rank: "queen",
 	suit: "hearts",
@@ -18,29 +18,45 @@ const cards = [
 	rank: "king",
 	suit: "diamonds",
 	cardImage: "images/king-of-diamonds.png"
-},
+}
 ];
 const cardsInPlay = [];
 
-function checkForMatch(){
-	if (cardsInPlay.length === 2) {
+	function checkForMatch(){
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			console.log("You found a match!");
 		} 
 			else {
 				alert("Sorry, try again.");
 			}
+	};	
+
+
+	function flipCard(){
+		var cardId = this.getAttribute("data-id"); //this line of code should get the id of each card and is storing it in the variable cardId 
+		this.setAttribute('src', cards[cardId].cardImage);
+		cardsInPlay.push(cards[cardId].rank);
+		console.log("User flipped " + cards[cardId].rank);
+		console.log(cards[cardId].cardImage);
+		console.log(cards[cardId].suit);
+		if (cardsInPlay.length === 2) {
+		checkForMatch();
 	}
-}
 
-function flipCard(cardID){
-	console.log("User flipped " + cards[cardID].rank);
-	console.log(cards[cardID].suit);
-	console.log(cards[cardID].cardImage);
-	cardsInPlay.push(cards[cardID].rank);
-	checkForMatch();
-}
+	};
 
 
-flipCard(0);
-flipCard(2);
+	function createBoard(){
+			for(let i = 0; i < cards.length; i++){
+			var cardElement = document.createElement("img");
+			cardElement.setAttribute("src","images/back.png");
+			cardElement.setAttribute("data-id", i);
+			cardElement.addEventListener("click", flipCard);
+			document.getElementById("game-board").appendChild(cardElement);
+
+		}
+	}
+
+
+createBoard();
+
